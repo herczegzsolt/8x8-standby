e4a1f2c1100121d241a690520c3ab7c002e9bab7
Herczeg Zsolt <herczegzsolt@herczegzsolt.hu>
2018-03-14 12:39:51 +0100

    Add support for mycontactual.com domain

--- src/background-script.js
+++ src/background-script.js
@@ -1,8 +1,14 @@
 'use strict';
 var tabs = [];
 function idleNotify(state){
-	// Notify all 8x8 windows about status changes
+	// Notify all *.8x8.com windows about status changes
         chrome.tabs.query({"url":"https://*.8x8.com/*"},function(tabs){
+                tabs.forEach(function(tab){
+			chrome.tabs.sendMessage(tab.id,{"action":"status","status":state});
+                });
+        });
+	// Notify all *.mycontactual.com windows about status changes
+        chrome.tabs.query({"url":"https://*.mycontactual.com/*"},function(tabs){
                 tabs.forEach(function(tab){
 			chrome.tabs.sendMessage(tab.id,{"action":"status","status":state});
                 });
